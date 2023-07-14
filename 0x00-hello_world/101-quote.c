8c0931c07eaff2927dc25da64da18b8c0aec3e20
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-14 00:37:58 -0700

    correction done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
+
 /**
-* main - entry point
-* Return: always 1(failure)
+* main - Entry point
+* Return: 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			stdout);
 	return (1);
 }
