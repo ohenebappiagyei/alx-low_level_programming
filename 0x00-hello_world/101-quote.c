2296f61b46346f9a1480290d9a9eb6f031b15cd1
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-13 21:15:32 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 /**
-   * main - entry point
-   *Return: always 1(failure)
-  */
+* main - entry point
+* Return: always 1(failure)
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
