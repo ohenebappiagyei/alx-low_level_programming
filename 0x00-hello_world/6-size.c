f0681a958fe637cded8cc659b67fc0a2f0a1276b
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-13 20:53:06 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0
- */
+   * main - entry point
+   *Return: always 1(failure)
+  */
 int main(void)
-{	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	return (0);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+	return (1);
 }
-
-
-
-
