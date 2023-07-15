1d97c3dbba9fc2e637647050287251e57698fcb0
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-15 01:21:43 -0700

    multiple combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 8; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (k = j + 1; k < 9; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				if (i != j && i != k && j != k)
 				{
