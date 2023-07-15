f9e8772b824727ebd5c310cc883e5749a41b27ed
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-15 00:58:43 -0700

    its fun when you know

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,6 +18,12 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
