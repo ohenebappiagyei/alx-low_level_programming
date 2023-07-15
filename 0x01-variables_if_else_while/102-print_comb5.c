b6a1ef48a27f664bcc4140ad489b505185b39e0d
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-15 01:39:03 -0700

    finally

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point for program
+ * Return: 0 (successful)
+ * Description: Program prints all possible combinations of 2 2-digit numbers.
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i <= j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 99 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
