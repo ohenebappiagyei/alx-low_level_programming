60464d0255a84de6aa887f35ea269a6ab758acbc
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-14 23:26:37 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,34 +4,34 @@
 
 /**
  * main - Entry point
- * Return: 0 (Successful)
- * Description:The last digited is printed.
+ * Return: 0 (successfull)
+ * Description: The last digit is printed.
 */
+
 int main(void)
 {
-        int n;
+	int n;
+	int LastDigit;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        int LastDigit = n % 10;
+	LastDigit = n % 10;
 
-        /* if the last digit of n is greater than 5 */
-        if (LastDigit > 5)
-        {
-                printf("the last digit of %d is %d and is greater than 5\n", n, LastDigit);
-        }
-        /* if the last digit of n is 0 */
-        else if (LastDigit == 0)
-        {
-                printf("the last digit of %d is 0\n", n);
-        }
-        /* if the last digit of n is less than 6 and not 0 */
-        else if (LastDigit < 6)
-        {
-                printf("the last digit of %d is %d and is less than 6 and not 0\n",
-                        n, LastDigit);
-        }
-
-        return (0);
+	/* if the last digit of n is greater than 5*/
+	if (LastDigit > 5)
+	{
+		printf("the last digit of %d is %d and is greater than	5\n", n, LastDigit);
+	}
+	/* if the last digit of n is 0*/
+	else if (LastDigit == 0)
+	{
+		printf("the last digit of %d is 0\n", n);
+	}
+	/* if the last digit of n is less than 6 and not 0*/
+	else if (LastDigit < 6)
+	{
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
+	}
+	return (0);
 }
