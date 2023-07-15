a34b900c86646efc6a9a3739b2a18c689b490d62
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-15 14:52:05 -0700

    This should work

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i; j < 100; j++)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
 			if (i != 98 || j != 99)
 			{
