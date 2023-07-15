6a037ab5a3d237bed4ffddb7ec6ca036a22618b7
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-15 14:41:45 -0700

    software is eating the world.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,15 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 1; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
 			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
 
-			if (i != 99 || j != 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
