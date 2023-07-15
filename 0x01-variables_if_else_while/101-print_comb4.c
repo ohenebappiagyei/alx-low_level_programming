2c19c82ad729f3dccb4bb2b0d8ea12938b18b650
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-15 00:55:44 -0700

    its fun when you know

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (successful)
+ * Description: 3 digit combinations
+*/
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 7; i++)
+	{
+		for (j = i + 1; j < 8; j++)
+		{
+			for (k = j + 1; k < 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
