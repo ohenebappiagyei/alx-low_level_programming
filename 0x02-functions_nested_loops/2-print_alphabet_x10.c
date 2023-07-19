e26ad8f7a3bffdce4f6f42082e12007d7f09ab0b
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-18 18:10:11 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabets 10times
- * Return: 0 (successful)
+ * print_alphabet_x10 - print alphabet 10X
+ * Return: 0
 */
 
-void print_alphabet_10(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	char l;
