19f0a667499521984792dac6f91cc2373ae56232
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-18 19:20:04 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of a number
+ * @d: argument
+ * Return: x
+*/
+
+int print_last_digit(int d)
+{
+	int x;
+
+	x = d %10;
+
+	if (d < 0)
+			x = -x;
+	_putchar(x + 'O');
+	return(x);
+}
