f48585721a7b60e24ff3be64ef174794f90a7d41
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-19 10:00:06 -0700

    do hard things

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,15 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 0 && n < 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",");
+			printf(",");
 		}
-		printf("\n");
+	printf("\n");
 	}
 
 	else if (n < 0)
@@ -28,10 +28,21 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+			printf(", ");
+		}
+	printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			printf(",");
 		}
 		printf("\n");
 	}
+
 	else if (n == 98)
 		printf("%d\n", n);
 }
