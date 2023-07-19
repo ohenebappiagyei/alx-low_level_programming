2449abdfbaf5efd7f7f610b0e310b1fd1c3308f8
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-19 16:06:15 -0700

    the journey will be hard but we will make it

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_times_table - main entry
+ * @n: parameter
+ * Return: always 0
+*/
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar(',');
+			_putchar('\n');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+				if (prod <= 99)
+					_putchar (' ');
+				else if (prod <= 9)
+					_putchar (' ');
+				else if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
