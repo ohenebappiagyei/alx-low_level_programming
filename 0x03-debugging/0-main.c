03b2fc942fa192b97189e146b7807f9d9abfb0f5
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-20 04:49:27 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that print if integer is signed or not.
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is the README file for the debuggingproject. The projects are supposed to help me understand the concept of debugging
