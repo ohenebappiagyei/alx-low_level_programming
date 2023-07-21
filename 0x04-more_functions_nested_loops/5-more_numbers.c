6ea7e72b8c4e5f5836e0b9ee657fa256b8fac892
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-21 06:06:34 -0700

    now it should work

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar((i / 10) + '0');
+				_putchar('0' + j  / 10);
 			}
-			_putchar((i % 10) + '0');
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
