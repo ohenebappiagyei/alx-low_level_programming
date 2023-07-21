dc6649c793037a4bb58a8ad7c2a939207561c643
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-21 07:29:37 -0700

    sometimes second deadlines are worth it

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - a function that prints a square
+ * @size:parameter
+ * Return: On success 0
+*/
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{for (y = 1; y <= size; y++)
+			{
+				_putchar ('#');
+			}
+			if (x == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+_putchar('\n');
+}
