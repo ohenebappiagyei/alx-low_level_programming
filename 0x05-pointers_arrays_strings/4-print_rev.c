d2c144695f13c86781cdeaca3e2b30d4aedad7e2
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-24 16:37:05 -0700

    one day...

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: Pointer to the string to be printed in reverse.
+*/
+void print_rev(char *s)
+{
+	int lenght = _strlen(s);
+	int i;
+
+	for (i = lenght - 1; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,9 +15,6 @@ void print_number(int n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-
-
-
-
+void print_rev(char *s);
 
 #endif
