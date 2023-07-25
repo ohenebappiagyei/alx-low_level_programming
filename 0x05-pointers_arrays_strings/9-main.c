bbb8641c5af6d0be22bd6455518421e0d77b28cf
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-25 10:52:11 -0700

    done alas

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -7,9 +7,22 @@
  */
 int main(void)
 {
-	char *str;
-
-	str = "0123456789";
-	puts2(str);
+	char s1[98];
+	char *ptr;
+	int i = 0;
+	
+	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	/* Replace printf with _putchar to print each character individually */
+	while (s1[i] != '\0')
+	{
+		_putchar(s1[i]);
+		i++;
+	}
+	i = 0;
+	while (ptr[i] != '\0')
+	{
+		_putchar(ptr[i]);
+		i++;
+	}
 	return (0);
 }
