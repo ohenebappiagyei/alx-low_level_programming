cb699cb24314c588329f3b1fc95efc7442d8e72a
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-24 17:15:44 -0700

    corrected

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * main - check the code 
+ * main - check the code
  *
- * Return: Always 0.
+ * Return: ALways 0.
 */
 int main(void)
 {
 	char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	str = "I do not fear computers. I  fear the lack of them - Isaac Asimov";
 	print_rev(str);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,34 @@
 #include "main.h"
+
+/**
+ * strlen - Returns the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string (excluding the null terminator)
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	
+	return length;
+}
+
 /**
  * print_rev - Prints a string in reverse followed by a new line.
  * @s: Pointer to the string to be printed in reverse.
 */
 void print_rev(char *s)
 {
-	int lenght = _strlen(s);
+	int length = _strlen(s);
 	int i;
 
-	for (i = lenght - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
