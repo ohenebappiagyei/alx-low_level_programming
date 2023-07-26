dadfec87c10859f19d291916d119abee08bcb071
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-26 12:21:05 -0700

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,6 +3,7 @@
  * *_strncpy - copies a string
  * @dest: appends the copied string here
  * @src: source string copied
+ * @n: The maximum number of characters to copy.
  * Return: (dest)
 */
 char *_strncpy(char *dest, char *src, int n)
