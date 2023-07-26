226869d4392645c43ba1540fa3fd3fe8298079b4
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-26 11:46:55 -0700

    its fun

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - The file for all the prototypes for the pointers,
+ * arrays, and strings
+ * Author: Dr. Appiagyei
+ * For ALX
+ * Return: nothing
+*/
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+
+
+#endif
