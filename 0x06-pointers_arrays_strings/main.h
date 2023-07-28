42ebbcdd0fe3740535dca4df0c58aa6d0379104f
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-27 21:09:24 -0700

    optional tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,5 +20,7 @@ char *leet(char *);
 char *rot13(char *s);
 void print_number(int n);
 int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
