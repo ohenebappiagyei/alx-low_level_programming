e0f9ed6df56f6bb2e7ee6f64042b41dcb256bd1d
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-07-31 16:29:33 -0700

    it will be alright

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+We continue with the project on pointers, arrays and strings to get a better understanding
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - The file for all the prototypes for the pointers,
+ * arrays, and strings
+ * Author: Dr. Appiagyei
+ * For ALX
+ * Return: nothing
+*/
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
