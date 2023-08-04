e2504a0ad17e6d13924217d98bf84a5431b193ed
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-03 22:27:30 -0700

    done1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints its name, followed by a new ine.
+ * @argc: Number of command line arguments SB1
+ * @argv: Command line arguments
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
