3e9a3d2126dfd584da0d049ac82e970343f4cd79
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-07 14:08:49 -0700

    it will turn out right

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * @create_array - Creates an array of chars
+ * and initializes it with a specific char
+ * @c: The character used to initialize each element of the array.
+ *
+ * Return: If size is 0, returns NULL. Otherwise, returns a pointer to the created array.
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	
+	array = (char *)malloc(size * sizeof(char));
+	
+	if (array == NULL)
+	{
+		/* Memory allocation failed */
+		return NULL;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return array;
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This is the file for the prototypes
+ * Author: Dr. Appiagyei
+ * For ALX
+*/
+char *create_array(unsigned int size, char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif
