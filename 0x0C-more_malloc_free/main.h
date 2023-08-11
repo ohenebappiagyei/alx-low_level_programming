dbf10b8789f48b44b2ce069fab37bab8591b8c70
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-10 21:48:09 -0700

    definitely i tried

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdbool.h>
 /**
  * This is the header file for all the prototypes for this task
  * Author: Dr. Appiagyei
@@ -10,6 +11,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+bool is_valid_number(const char *str);
+void print_number(int num);
 
 
 #endif
