a9860b909083c61490efac15397ba37de5d5359c
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-14 17:39:27 -0700

    all of me

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,13 @@
  *
  *This structure holds information about a dog
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
