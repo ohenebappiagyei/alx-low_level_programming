097bbe3081ab329fa18db76f1a64f1d66ed37519
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-16 16:52:39 -0700

    due diligence

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - Returns the sum of a and b
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The sum of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns the difference between a and b
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The difference between a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of a and b
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The product of a and b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the result of the division of a by b
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result of the division of a by b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of a by b
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
