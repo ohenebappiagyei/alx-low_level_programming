1479b1a977be88a117fb6b97596bf7a89caab86e
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-24 14:37:10 -0700

    add node function added

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Definition of  the list_t structure */
 typedef struct list_s
@@ -16,5 +18,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
