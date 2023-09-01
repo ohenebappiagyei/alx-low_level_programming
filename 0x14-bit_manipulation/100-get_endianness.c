0461ef6992ccc49af7f28b8ff5776045e906ab68
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-31 20:32:33 -0700

    because of this thing

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness - Check the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	unsigned char *byte_ptr = (unsigned char *)&num;
+
+	return (*byte_ptr);
+}
