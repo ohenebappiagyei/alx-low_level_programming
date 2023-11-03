266519f5484f67d5884db1401379deca96156274
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-03 05:11:12 -0700

    correction

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,9 +6,10 @@
  * @size: The size of the array of the hash table
  *
  * Return: The index at which the key should be stored in the hash table.
-*/ 
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value = hash_djb2(key);
+
 	return (hash_value % size);
 }
