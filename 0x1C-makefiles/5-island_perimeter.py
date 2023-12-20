7f60ba7916f3a07dacd9f0c3dd84424bbddcfeb2
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-12-20 12:09:16 -0800

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid.
+
+    Args:
+        grid (list): List of lists of integers representing the island.
+
+    Returns:
+        int: Perimeter of the island.
+
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                # Check top
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
